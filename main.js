6edf5cddfc96bc1c08a5dfac420d9e29c4a1c997
Vincentvdt <vincent.vidot3@gmail.com>
2023-02-13 20:32:18 +0100

    change things

--- main.js
+++ main.js
@@ -1,131 +1,178 @@
-document.addEventListener("DOMContentLoaded", () => {
+// Configuration
+let mouse = {
+    x: 0, y: 0
+}
+const friction = .98
 
-    const root = document.querySelector('#root');
-    const message = root.querySelector('p span');
+document.addEventListener('mousemove', ({clientX, clientY}) => {
+    mouse.x = clientX
+    mouse.y = clientY
+})
+const canvas = document.getElementById('game');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
+const ctx = canvas.getContext('2d');
 
-    function calcAngle(origin, p2) {
-        let dx = p2.x - origin.x;
-        let dy = p2.y - origin.y;
-
-        return Math.atan2(dy, dx) * 180 / Math.PI
+class Player {
+    constructor(x, y, radius, color) {
+        this.position = {
+            x: x, y: y
+        }
+        this.radius = radius
+        this.color = color
     }
 
-    class Turret {
-
-        constructor(posX, posY) {
-            this.turretDOM = "";
-            this.id = Math.floor(Math.random() * Date.now()).toString();
-            this.posX = posX;
-            this.posY = posY;
-            this.size = Math.floor(Math.random() * (50 - 20) + 20);
-            this.diameter = this.size * (Math.floor(Math.random() * (10 - 5) + 5));
-        }
-
-
-        init() {
-            let newTurret = this.createDomElement();
-            root.appendChild(newTurret);
-            this.turretDOM = document.querySelector(`.turret[data-id="${this.id}"]`);
-            this.addEvent(this.turretDOM);
-        }
-
-        destroy(turret) {
-            root.removeChild(turret.parentNode);
-            console.log(`You killed : ${turret.parentNode.dataset.id}`);
-        }
-
-        addEvent(turret) {
-            let turretZone = turret.querySelector('.zone');
-
-            turretZone.addEventListener('mouseover', () => {
-                this.turretDOM.classList.add("triggered");
-            })
-
-            turretZone.addEventListener('mouseout', () => {
-                this.turretDOM.classList.remove("triggered");
-            })
-
-            turret.addEventListener('click', (e) => {
-                e.stopPropagation();
-                this.destroy(e.target);
-            })
-        }
-
-        createDomElement() {
-            let turretDOM = document.createElement("div");
-            turretDOM.dataset.id = this.id;
-
-            turretDOM.setAttribute("class", "turret");
-            turretDOM.setAttribute("style", `
-                width: ${this.size}px;
-                height: ${this.size}px; 
-                top:${this.posY}px; 
-                left:${this.posX}px;`);
-
-            let aggroArea = document.createElement("div");
-            aggroArea.setAttribute("class", "zone");
-            turretDOM.appendChild(aggroArea)
-
-
-            turretDOM.querySelector('.zone').setAttribute('style',
-                `width: ${this.diameter}px; height: ${this.diameter}px;`)
-
-            turretDOM.innerHTML += `<p>${this.id}</p>`
-
-            return turretDOM;
-        }
-
+    draw() {
+        ctx.beginPath();
+        ctx.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2, false);
+        ctx.fillStyle = this.color
+        ctx.fill();
     }
 
-    root.addEventListener("click", (e) => {
-        let newTurret = new Turret(e.pageX, e.pageY);
-        newTurret.init()
-    })
+    update() {
+        this.position.x = mouse.x
+        this.position.y = mouse.y
+        this.draw()
+    }
 
+}
 
-    document.addEventListener("mousemove", (e) => {
-        let hoveredTurret = allElementsFromPoint(e.pageX, e.pageY);
-        let msg = [];
-        if (hoveredTurret) {
-            let parentNode = hoveredTurret.map(turret => turret.parentNode);
-            parentNode.forEach(turret => {
-                msg.push(turret.dataset.id)
-                let deg = Math.floor((
-                    calcAngle({
-                        x: turret.offsetLeft,
-                        y: turret.offsetTop
-                    }, {
-                        x: e.pageX,
-                        y: e.pageY
-                    }) + 360) % 360);
-
-                turret.style.transform = `rotate(${deg}deg)`
-            })
+class Projectile {
+    constructor(x, y, radius, color, velocity) {
+        this.position = {
+            x: x, y: y
         }
-        message.innerText = msg;
-    })
+        this.radius = radius
+        this.color = color
+        this.velocity = {
+            x: velocity.x, y: velocity.y
+        }
+    }
 
+    draw() {
+        ctx.beginPath()
+        ctx.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2, false)
+        ctx.fillStyle = this.color
+        ctx.fill()
+    }
 
-    function allElementsFromPoint(x, y) {
-        let element, elements = [];
-        let old_visibility = [];
-        while (true) {
-            element = document.elementFromPoint(x, y);
-            if (!element || element === document.documentElement || !element.classList.contains("zone")) {
-                break;
+    update() {
+        this.draw()
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+    }
+
+}
+
+class Turret {
+    constructor(x, y, radius, color) {
+        this.position = {
+            x: x, y: y
+        }
+        this.radius = radius
+        this.baseColor = color
+        this.color = this.baseColor
+        this.range = 200
+
+        this.ang = Math.random() * 2 * Math.PI;
+        this.lastShotTime = Date.now() + Math.random() * (1000 - 200) + 200
+    }
+
+    shouldShoot() {
+        return (Date.now() - this.lastShotTime >= 1000 +  Math.random() * (1000 - 200) + 200)
+    }
+
+    shot() {
+        if (this.shouldShoot()) {
+            this.lastShotTime = Date.now()
+            let velocity = {
+                x: Math.cos(this.ang) * -1,
+                y: Math.sin(this.ang) * -1
             }
-            elements.push(element);
-            old_visibility.push(element.style.visibility);
-            element.style.visibility = 'hidden'; // Temporarily hide the element (without changing the layout)
-        }
-        for (let k = 0; k < elements.length; k++) {
-            elements[k].style.visibility = old_visibility[k];
-        }
-        elements.reverse();
-        if (elements.length) {
-            return elements
+
+            projectiles.push(new Projectile(
+                this.position.x + (this.radius + 15) * (Math.cos(this.ang) * -1),
+                this.position.y + (this.radius + 15) * (Math.sin(this.ang) * -1),
+                8,
+                "#DABFFF",
+                {
+                x :velocity.x * 1.1,
+                y: velocity.y * 1.1
+            }))
         }
     }
 
-});
+    draw() {
+        ctx.fillStyle = this.color
+        ctx.strokeStyle = this.color
+        ctx.beginPath();
+        ctx.arc(this.position.x, this.position.y, this.radius, 0, 2 * Math.PI);
+        ctx.fill();
+        ctx.beginPath();
+        ctx.moveTo(this.position.x, this.position.y);
+        ctx.lineTo((this.position.x + (this.radius + 10) * (Math.cos(this.ang) * -1)), this.position.y + (this.radius + 10) * (Math.sin(this.ang) * -1));
+        ctx.lineWidth = 20;
+        ctx.stroke();
+
+        // ctx.beginPath();
+        // ctx.moveTo(this.position.x, this.position.y);
+        // ctx.lineTo(mouse.x, mouse.y);
+        // ctx.setLineDash([5, 10]);  // Set the line dash to [2, 2]
+        // ctx.lineWidth = .5;
+        // ctx.stroke();
+        // ctx.setLineDash([]);  // Reset the line dash to solid line
+
+    }
+
+    update() {
+        let distance = Math.sqrt(Math.pow(this.position.x - mouse.x, 2) + Math.pow(this.position.y - mouse.y, 2));
+        if (distance < this.range) {
+            this.ang = Math.atan2(this.position.y - mouse.y, this.position.x - mouse.x)
+            this.color = "#E54B4B"
+            this.shot()
+        } else {
+            this.color = this.baseColor
+        }
+        this.draw()
+    }
+}
+
+let turrets = [];
+let projectiles = []
+let player = new Player(window.innerWidth / 2, window.innerHeight / 2, 20, "#9055A2")
+
+while (turrets.length < 20) { // Number of turrets you want to generate
+    let x = Math.floor(Math.random() * (canvas.width - 2 * 30)) + 30;
+    let y = Math.floor(Math.random() * (canvas.height - 2 * 30)) + 30;
+
+    let isTooClose = false;
+    for (let i = 0; i < turrets.length; i++) {
+        let distance = Math.sqrt(Math.pow(x - turrets[i].position.x, 2) + Math.pow(y - turrets[i].position.y, 2));
+        if (distance < 200) {
+            isTooClose = true;
+            break;
+        }
+    }
+    if (!isTooClose) {
+        turrets.push(new Turret(x, y, 30, "#FAF3DD"));
+    }
+}
+
+
+function animate() {
+    requestAnimationFrame(animate)
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    projectiles.forEach((projectile, projectileIndex) => {
+        if (projectile.position.x < 0 || projectile.position.x > canvas.width || projectile.position.y < 0 || projectile.position.y > canvas.height) {
+            projectiles.splice(projectileIndex, 1);
+
+        }
+        projectile.update();
+    })
+    turrets.forEach(turret => turret.update())
+    player.update()
+}
+
+animate()
